09b2a5dc8e570de951b6ae96430b52c4a1f4459f
Tomas Barrios <tomasbarrios@gmail.com>
2023-06-14 21:51:25 -0400

    revert netlify
    getting an error
    https://discord.com/channels/830184174198718474/1094738070139580416/1094738070139580416

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify/functions";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: netlify()
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
